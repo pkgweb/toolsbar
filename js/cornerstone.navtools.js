da71056f22d21ef6ac7eac4bfc905c035035666c
gary guo <gary@pkgweb.com>
2016-02-27 13:56:22 +0800

    update

--- js/cornerstone.navtools.js
+++ js/cornerstone.navtools.js
@@ -1,12 +1,20 @@
-jQuery(document).ready(function(){
-    jQuery("#cornerstone-tools .profile-info").click(function(){
-        jQuery(".profile-menu").toggleClass( "toggled");
+(function(window, document, $){
+$(document).ready(function(){
+    $("#cornerstone-tools .profile-info").click(function(){
+        $(".profile-menu").toggleClass( "toggled");
         return false;
     });
 
-    jQuery("#navtools .expanded").click(function(){
-    	jQuery(this).toggleClass( "toggled");
+    $("#navtools .expanded").click(function(){
+    	$(this).toggleClass( "toggled");
         return false;
     });
+
+    $(".dropdown-menu li").on('click', 'a', function(e){ 
+    e.preventDefault(); 
+    var url = $(this).attr('href'); 
+    window.open(url, '_blank');
+	});
 
  });
+})(window, document, jQuery);
