4a00c0723dc74bf5ccd662e2b6766cdeedf56aef
pkgweb <gary@pkgweb.com>
2016-03-01 10:38:17 +0800

    Update cornerstone.navtools.js

--- js/cornerstone.navtools.js
+++ js/cornerstone.navtools.js
@@ -5,16 +5,16 @@ $(document).ready(function(){
         return false;
     });
 
-    $("#navtools .expanded > a").click(function(){
+    $("#navtools .expanded ").click(function(){
     	$(this).toggleClass( "toggled");
         return false;
     });
 
- //   $(".dropdown-menu li").on('click', 'a', function(e){ 
- //   e.preventDefault(); 
- //   var url = $(this).attr('href'); 
- //   window.open(url, '_blank');
-	// });
+    $(".dropdown-menu li").on('click', 'a', function(e){ 
+    e.preventDefault(); 
+    var url = $(this).attr('href'); 
+    window.open(url, '_blank');
+	});
 
  });
 })(window, document, jQuery);
